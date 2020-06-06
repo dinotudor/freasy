0eb0723aa320fd9b3e24ed869be84c60a1db282d
Dino Tudor <dinotudor@gmail.com>
2020-06-06 20:51:36 -0300

    Footer copyright and get year

--- footer.php
+++ footer.php
@@ -13,17 +13,9 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'freasy' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'freasy' ), 'WordPress' );
-				?>
-			</a>
+			<span>Freasy - Comida Congelada</span>
 			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'freasy' ), 'freasy', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			<span class="copy-date"><?php echo date('Y'); ?> &copy; Copyright - Freasy</span>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
